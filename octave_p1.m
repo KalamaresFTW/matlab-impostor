009b0d0861681d9ee8927f793033b155297f4dcc
araghunde <paybol.21@gmail.com>
2016-09-22 11:11:15 +0200

    added p1.m

---
+++ octave_p1.m
@@ -0,0 +1,18 @@
+%%ejercicio 1.1
+
+(3^2 + 7) / 2
+%%ans = 8
+2^3 - (16 * (7 + 24/3)) / (2^4 * 5)
+%%ans = 5
+(625)^(1/4)
+%%ans = 5.000
+%%separando con ',' devuelve las dos evaluaciones
+%%separando con ';' devuelve la ultima evaluacion (que no tiene ';' al final)
+%%format long
+%%format short
+%%format rat
+format short; 2/3, 1.44444
+format long; 2/3, 1.44444
+format long e; 2/3, 1.44444
+format rat; 2/3, 1.44444
+
