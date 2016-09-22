d8338aeb3bec3766bc78c20d3a2e8bab1f5609b0
araghunde <paybol.21@gmail.com>
2016-09-22 11:25:53 +0200

    pushing

--- octave_p1.m
+++ octave_p1.m
@@ -11,8 +11,41 @@
 %%format long
 %%format short
 %%format rat
+
+%%ejercicio 1.2
 format short; 2/3, 1.44444
 format long; 2/3, 1.44444
 format long e; 2/3, 1.44444
 format rat; 2/3, 1.44444
 
+%%abs -> valor absoluto
+%%rat -> cociente que aproxima el numero
+%%sqrt -> raiz cuadrada
+%%exp -> e^x
+%%log -> logaritmo neperiano
+%%log10 -> logaritmo base 10
+%%log2 -> logaritmo base 2
+
+%%sin -> seno
+%%cos -> coseno
+%%tan -> tangente
+%%csc -> cosecante
+%%sec -> secante
+%%cot -> cotangente
+%%asin -> arcoseno
+%%acos -> arcocoseno
+%%atan -> aroctangente
+
+%%%ARREPRESENTAÇOM GRÂFICA
+v = [3 6 2 1 5] %vector
+v'              %vector columna
+
+%%ejercicio 1.4
+v = [3 6 2 7 1 5] %hace un vector v
+v(3)              %devuelve la tercera posición del vector
+v([2,4])          %devuelve las posiciones 2 y 4 del vector
+zeros (1,5);      %hace un vector tamaño 5 con 5 ceros
+linspace(2, 10, 3)%devuelve un vector con tres posiciones
+                  %con numeros equidistantes entre 2 y 10
+
+                  
