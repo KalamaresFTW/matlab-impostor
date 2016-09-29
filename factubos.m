862f71842fa0f2241a504bdac0ecabdccac72513
araghunde <paybol.21@gmail.com>
2016-09-29 11:50:49 +0200

    fixed octave syntaxis to match matlab's

--- factubos.m
+++ factubos.m
@@ -1,7 +1,7 @@
 function factorialesYcubos(n)
   if rem(n,2) == 0 && n <= 8
     factorial(n)
-  elseif rem(n,2) != 0 && n<= 9
+  elseif rem(n,2) ~= 0 && n<= 9
     n^3
   else disp ('el numero debe ser menor que 9')
   endif
